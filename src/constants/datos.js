0cf3373c107034733e809332f2025041fbd8158a
sauliyo15 <gc.saul.7@gmail.com>
2024-01-19 13:02:57 +0100

    Archivo json de datos creado

---
+++ src/constants/datos.js
@@ -0,0 +1,9 @@
+export const datos = {
+    personales : {
+        nombre: "Saúl",
+        apellidos: "García Calvo",
+        nickname: "@sauliyo15",
+        descripcion: "Lorem Ipsum es un texto de marcador de posición comúnmente utilizado en las industrias gráficas, gráficas y editoriales para previsualizar diseños y maquetas visuales.",
+        urlImagen: "https://pbs.twimg.com/media/GEM-6u7WwAAqzsh?format=jpg&name=900x900"
+    }
+}
