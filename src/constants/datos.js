a0f9a6dca05f04d8957910aef0a3c493edea7031
sauliyo15 <gc.saul.7@gmail.com>
2024-01-23 11:04:56 +0100

    json de datos ampliado

--- src/constants/datos.js
+++ src/constants/datos.js
@@ -4,6 +4,104 @@ export const datos = {
         apellidos: "García Calvo",
         nickname: "@sauliyo15",
         descripcion: "Lorem Ipsum es un texto de marcador de posición comúnmente utilizado en las industrias gráficas, gráficas y editoriales para previsualizar diseños y maquetas visuales.",
-        urlImagen: "https://pbs.twimg.com/media/GEM-6u7WwAAqzsh?format=jpg&name=900x900"
-    }
+        urlImagen: "/imagenes/foto_personal.jpg"
+    },
+    cursos: [
+        {
+            nombre: "Frontend",
+            descripcion: "Desarrollo Frontend con HTML, CSS y JavaScript",
+            edicion: "3ª Edición - 2023",
+            modalidad: "ONLINE",
+            entidad: "Universidad Politécnica de Madrid",
+            duracion: "56 horas",
+            urlImagen: "/imagenes/frontend.png",
+            enlace: "https://www.google.com"
+        },
+        {
+            nombre: "Git y GitHub",
+            descripcion: "Gestión de proyectos Software con Git y GitHub",
+            edicion: "3ª Edición - 2023",
+            modalidad: "ONLINE",
+            entidad: "Universidad Politécnica de Madrid",
+            duracion: "56 horas",
+            urlImagen: "/imagenes/gitgithub.png",
+            enlace: "https://www.google.com"
+        },
+        {
+            nombre: "Backend",
+            descripcion: "Desarrollo Backend con Node.js, Expres y BB.DD",
+            edicion:  "3ª Edición - 2023",
+            modalidad: "ONLINE",
+            entidad: "Universidad Politécnica de Madrid",
+            duracion: "56 horas",
+            urlImagen: "/imagenes/backend.png",
+            enlace: "https://www.google.com"
+        },
+        {
+            nombre: "Full Stack",
+            descripcion: "Desarrollo de un proyecto Full Stack con JavaScript",
+            edicion:  "3ª Edición - 2023",
+            modalidad: "ONLINE",
+            entidad: "Universidad Politécnica de Madrid",
+            duracion: "64 horas",
+            urlImagen: "/imagenes/fullstack.jpg",
+            enlace: "https://www.google.com"
+        },
+        {
+            nombre: "Bases de Datos Relacionales y No Relacionales",
+            descripcion: "Conceptos, diseño y aplicación a entornos web",
+            edicion: "2ª Edición - 2023",
+            modalidad: "ONLINE",
+            entidad: "Universidad Politécnica de Madrid",
+            duracion: "60 horas",
+            urlImagen: "/imagenes/basededatos.png",
+            enlace: "https://www.google.com"
+        },
+        {
+            nombre: "React y React Native",
+            descripcion: "Desarrolo de aplicaciones con React y React Native",
+            edicion: "2ª Edición - 2023",
+            modalidad: "ONLINE",
+            entidad: "Universidad Politécnica de Madrid",
+            duracion: "75 horas",
+            urlImagen: "/imagenes/react.png",
+            enlace: "https://www.google.com"
+        },
+        {
+            nombre: "Java - IFCT034PO",
+            descripcion: "Desarrollo de aplicaciones con Java",
+            edicion: "Noviembre - Diciembre 2023",
+            modalidad: "ONLINE",
+            entidad: "Servicio Público de Empleo Estatal",
+            duracion: "120 horas",
+            urlImagen: "/imagenes/java.png",
+            enlace: "https://www.google.com"
+        }
+    ],
+    contacto: [
+        {
+            nombre: "Correo electrónico",
+            descripcion: "gc.saul.7@gmail.com",
+            urlImagen: "/imagenes/email.png",
+            enlace: "mailto:gc.saul.7@gmail.com"
+        },
+        {
+            nombre: "Teléfono y Whatsapp",
+            descripcion: "665 69 79 48",
+            urlImagen: "/imagenes/whatsapp.png",
+            enlace: "https://wa.me/665697948"
+        },
+        {
+            nombre: "Instagram",
+            descripcion: "@sauliyo15",
+            urlImagen: "/imagenes/instagram.png",
+            enlace: "https://www.instagram.com/sauliyo15/",
+        },
+        {
+            nombre: "GitHub",
+            descripcion: "https://github.com/sauliyo15",
+            urlImagen: "/imagenes/github.png",
+            enlace: "https://github.com/sauliyo15"
+        }
+    ],
 }
