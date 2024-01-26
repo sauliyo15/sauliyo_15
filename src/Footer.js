39fc5e173123bac87f2630622b77be46e01176ba
sauliyo15 <gc.saul.7@gmail.com>
2024-01-26 12:22:57 +0100

    Componente Footer añadido

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer(props) {
+    return(
+        <div className="centered-content3">
+          <img src="/imagenes/letra_s.png" alt="Copyright"></img>
+          <p>© 2024 sauliyo15.glitch.me by Saúl García v1.</p>
+        </div>
+    );
+}
