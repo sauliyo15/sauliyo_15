a9e4b7d1cc580dc9d49077f7322db9487ecf8a24
sauliyo15 <gc.saul.7@gmail.com>
2024-01-31 12:50:15 +0100

    Cambios renombrando componentes

--- src/App.js
+++ src/App.js
@@ -1,64 +1,85 @@
 import "./App.css";
 import { datos } from "./constants/datos";
 import Header from "./Header";
-import DatosPersonales from "./DatosPersonales";
-import DatosPersonales2 from "./DatosPersonales2";
+import Footer from "./Footer";
+import Element from "./Element";
+import NotFound from "./NotFound";
+import Titulos from "./Titulos";
+import Datos from "./Datos";
 import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   const [resultados, setResultados] = useState(null);
   const [cargando, setCargando] = useState(true);
 
   useEffect(() => {
-    
     const fetchData = () => {
-      setTimeout(()=> {
+      setTimeout(() => {
         setResultados(datos);
-        setCargando(false); 
-      }, 3000)             
+        setCargando(false);
+      }, 3000);
     };
 
     fetchData();
-
   }, []);
-  
+
   return (
     <div>
       <Header />
-      <div className="content-container">
-        <DatosPersonales
-          titulo={datos.personales.nombre + " " + datos.personales.apellidos}
-          nickname={datos.personales.nickname}
-          descripcion={datos.personales.descripcion}
-          imageUrl={datos.personales.urlImagen}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="content-container">
+              <Titulos
+                titulo={
+                  datos.personales.nombre + " " + datos.personales.apellidos
+                }
+                nickname={datos.personales.nickname}
+                descripcion={datos.personales.descripcion}
+                imageUrl={datos.personales.urlImagen}
+              />
+
+              {cargando && (
+                <img
+                  className="cargando"
+                  src="/imagenes/cargando.gif"
+                  alt="cargando"
+                ></img>
+              )}
+
+              {!cargando && (
+                <Datos
+                  titulo="Mis estudios realizados"
+                  datos={resultados.cursos}
+                />
+              )}
+
+              {!cargando && (
+                <Datos
+                  titulo="Algunos de mis proyectos y aplicaciones"
+                  datos={resultados.proyectos}
+                />
+              )}
+
+              {!cargando && (
+                <Datos
+                  titulo="Contacto y recursos disponibles"
+                  datos={resultados.contacto}
+                />
+              )}
+              <Footer />
+            </div>
+          }
         />
-
-        {cargando && (
-          <img className="cargando" src="/imagenes/cargando.gif" alt="cargando"></img>
-        )}
-
-        {!cargando && (<DatosPersonales2
-          titulo="Mis estudios realizados"
-          datos={resultados.cursos}
-        />)}
-
-        {!cargando && (<DatosPersonales2
-          titulo="Algunos de mis proyectos y aplicaciones"
-          datos={resultados.proyectos}
-        />)}
-
-        {!cargando && (<DatosPersonales2
-          titulo="Contacto y recursos disponibles"
-          datos={resultados.contacto}
-        />)}
         
-        
-        <div className="centered-content3">
-          <img src="/imagenes/letra_s.png" alt="Copyright"></img>
-          <p>© 2024 sauliyo15.glitch.me by Saúl García v1.</p>
-        </div>
-      </div>
+        <Route path="/proyectos/:id" element={<Element datos={datos.proyectos}/>}
+        />
+        <Route path="/cursos/:id" element={<Element datos={datos.cursos}/>}
+        />
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </div>
   );
 }
