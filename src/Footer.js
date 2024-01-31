91e397a9603d1b54ca2d7cf5179021637919f4e9
sauliyo15 <gc.saul.7@gmail.com>
2024-01-31 17:49:03 +0100

    Cambios necesarios para desplegar en Firebase

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ export default function Footer(props) {
     return(
         <div className="centered-content3">
           <img src="/imagenes/letra_s.png" alt="Copyright"></img>
-          <p>© 2024 sauliyo15.glitch.me by Saúl García v1.</p>
+          <p>© 2024 sauliyo15.web.app by Saúl García v1.</p>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,28 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD7QxC3pUEi__1kEeTWS0QIZkICcdBMaF4",
+  authDomain: "sauliyo15-63895.firebaseapp.com",
+  projectId: "sauliyo15-63895",
+  storageBucket: "sauliyo15-63895.appspot.com",
+  messagingSenderId: "1081952556697",
+  appId: "1:1081952556697:web:e080482a1e8d8b0f5aca08",
+  measurementId: "G-YWQJKYKB0F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
