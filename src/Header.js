843db7c6453671a4f1ad2ed1487ee4814c75da91
sauliyo15 <gc.saul.7@gmail.com>
2024-02-06 18:42:04 +0100

    Link cabecera al inicio

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header(props) {
-    return(
-        <div className='header'>
-            <span className='nombre'>sauliyo</span>
-            <span className='numero'>15</span>
-        </div>
-    );
+  return (
+    <div className="header">
+      <Link className="link" to="/">
+        <span className="nombre">sauliyo</span>
+        <span className="numero">15</span>
+      </Link>
+    </div>
+  );
 }
