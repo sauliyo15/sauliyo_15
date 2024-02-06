cf7698eccac76fb332904d6e0d3e5b18c6102d60
sauliyo15 <gc.saul.7@gmail.com>
2024-02-06 18:47:32 +0100

    Link Footer repositorio GitHub

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer(props) {
     return(
         <div className="centered-content3">
           <img src="/imagenes/letra_s.png" alt="Copyright"></img>
+          <Link className="link" to="https://github.com/sauliyo15/sauliyo_15" target='_blank'>
           <p>© 2024 sauliyo15.web.app by Saúl García v1.</p>
+          </Link>
         </div>
     );
 }
